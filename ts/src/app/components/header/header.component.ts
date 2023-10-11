1868da64221ac2174446222adac839e01430feaa
Matheus Lima <teulima20@hotmail.com>
2023-10-11 16:42:04 -0300

    feat: added all styles and features

--- ts/src/app/components/header/header.component.ts
+++ ts/src/app/components/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  mobileMenuVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuVisible = !this.mobileMenuVisible;
   }
 
 }
--- ts/src/app/components/main-discussoes/main-discussoes.component.ts
+++ ts/src/app/components/main-discussoes/main-discussoes.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-discussoes.component.scss']
 })
 export class MainDiscussoesComponent implements OnInit {
+  formVisible = false;
+  successVisible = false;
+  presentationVisible = true;
+  hiddenVisible = false;
+  answerVisible = false;
+  likeCount = 1;
+  mobileMenuVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleForm() {
+    this.formVisible = !this.formVisible;
+    this.presentationVisible = !this.formVisible;
+  }
+
+  toggleSuccess() {
+    this.successVisible = !this.successVisible;
+    this.hiddenVisible = this.successVisible;
+    this.formVisible = !this.successVisible;
+  }
+
+  toggleNewTopic() {
+    this.formVisible = !this.formVisible;
+    this.hiddenVisible = !this.formVisible;
+    this.successVisible = false;
+  }
+
+  toggleAnswer() {
+    this.answerVisible = !this.answerVisible;
+    this.likeCount = this.answerVisible ? 4 : 1;
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuVisible = !this.mobileMenuVisible;
+  }
+
+  onResize() {
+    if (document.body.clientWidth > 769) {
+      this.mobileMenuVisible = false;
+    }
+  }
 }
